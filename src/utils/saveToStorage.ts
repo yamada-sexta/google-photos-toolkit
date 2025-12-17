65ba879133d8fce572037a89dd45a360c655f2b5
Yamada <114621472+yamada-sexta@users.noreply.github.com>
2025-12-16 21:55:41 -0600

    More typescript

--- src/utils/saveToStorage.ts
+++ src/utils/saveToStorage.ts
@@ -1,8 +1,8 @@
 import { windowGlobalData } from '../windowGlobalData.js';
 
-export default function saveToStorage(key, value) {
+export default function saveToStorage(key: string, value: any): void {
   if (typeof Storage !== 'undefined') {
-    let userStorage = JSON.parse(localStorage.getItem(windowGlobalData.account)) || {};
+    let userStorage = JSON.parse(localStorage.getItem(windowGlobalData.account) || "{}") || {};
     userStorage[key] = value;
     localStorage.setItem(windowGlobalData.account, JSON.stringify(userStorage));
     console.log('Data saved to localStorage:', key);
